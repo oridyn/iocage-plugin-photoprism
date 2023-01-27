96b3629fde3c7b85af6404a40e47edc273d51719
Daniel Nyiro <d.nyiro@gmail.com>
2023-01-27 14:58:50 +0000

    Generate the Admin password during install

--- post_install.sh
+++ post_install.sh
@@ -17,24 +17,30 @@ DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 _EOF_
 
+# Generate some passwords
+export LC_ALL=C
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/db_password
+DB_PASSWORD=`cat /root/db_password`
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/admin_password
+ADMIN_PASSWORD=`cat /root/db_password`
+
+
+
 # Configure the DB
 # Create user and database for Piwigo with unique password
-USER="photoprism"
+DB_USER="photoprism"
 DB="photoprism"
 # Save the config values
-echo "$DB" > /root/dbname
-echo "$USER" > /root/dbuser
-export LC_ALL=C
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
-PASS=`cat /root/dbpassword`
-echo "Database User: $USER"
-echo "Database Password: $PASS"
+echo "$DB" > /root/db_name
+echo "$DB_USER" > /root/db_user
+echo "Database User: $DB_USER"
+echo "Database Password: $DBPASSWORD"
 mysql --user=root <<_EOF_
 CREATE DATABASE ${DB}
 CHARACTER SET = 'utf8mb4'
 COLLATE = 'utf8mb4_unicode_ci';
-CREATE USER '${USER}'@'%' IDENTIFIED BY '${PASS}';
-GRANT ALL PRIVILEGES ON ${DB}.* to '${USER}'@'%';
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB}.* to '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 _EOF_
 
@@ -44,6 +50,7 @@ pkg add https://github.com/psa/libtensorflow1-freebsd-port/releases/download/1.1
 pkg add https://github.com/psa/photoprism-freebsd-port/releases/download/2022-11-18/photoprism-g20221118-FreeBSD-12.3-separatedTensorflow.pkg
 
 sysrc photoprism_enable="YES"
+sysrc photoprism_auth_mode="public"
 sysrc photoprism_assetspath="/var/db/photoprism/assets"
 sysrc photoprism_storagepath="/mnt/photos/"
 sysrc photoprism_defaultsyaml="/mnt/photos/options.yml"
@@ -51,7 +58,7 @@ sysrc photoprism_defaultsyaml="/mnt/photos/options.yml"
 mkdir /mnt/photos
 chown -R photoprism:photoprism /mnt/photos
 cat >/mnt/photos/options.yml <<EOL
-AuthMode: public #[OPTIONAL]
+AdminPassword: ${ADMIN_PASSWORD}
 AssetsPath: /var/db/photoprism/assets
 StoragePath: /mnt/photos
 OriginalsPath: /mnt/photos/originals
@@ -59,14 +66,15 @@ ImportPath: /mnt/photos/import
 DatabaseDriver: mysql
 DatabaseName: ${DB}
 DatabaseServer: "127.0.0.1:3306"
-DatabaseUser: ${USER}
-DatabasePassword: ${PASS}
+DatabaseUser: ${DB_USER}
+DatabasePassword: ${DB_PASSWORD}
 EOL
 
 service photoprism start
 
 # Add plugin detals to info file available in TrueNAS Plugin Additional Info
 echo "Host: 127.0.0.1" > /root/PLUGIN_INFO
-echo "Database User: $USER" >> /root/PLUGIN_INFO
-echo "Database Password: $PASS" >> /root/PLUGIN_INFO
+echo "Admin Password: $ADMIN_PASSWORD" >> /root/PLUGIN_INFO
+echo "Database User: $DB_USER" >> /root/PLUGIN_INFO
+echo "Database Password: $DB_PASSWORD" >> /root/PLUGIN_INFO
 echo "Database Name: $DB" >> /root/PLUGIN_INFO
